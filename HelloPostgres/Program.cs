1692d7970d563d41519f67f4b67bd7fdd6fa624c
fiach <fiach.reid@gmail.com>
2020-06-19 18:10:50 +0200

    Initial commit

---
+++ HelloPostgres/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Npgsql; // Install-Package Npgsql -Version 4.1.3.1
+
+namespace HelloPostgres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connString = "Host=crt.sh;Username=guest;Password=certwatch;Database=certwatch";
+            var conn = new NpgsqlConnection(connString);
+            conn.Open();
+            // List all certificate authorities
+            var cmd = new NpgsqlCommand("select name from ca", conn);
+            var reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine(reader["name"]);
+            }   
+            conn.Close();
+            Console.ReadLine();
+        }
+    }
+}
